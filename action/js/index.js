d97080b4c75d8dd6bb366f18a3f99f2bc51caeef
Raihan hosen <83702632+raihanhosen011@users.noreply.github.com>
2021-07-26 08:09:41 +0600

    Add files via upload

---
+++ action/js/index.js
@@ -0,0 +1,8 @@
+// dependency
+const caollapsed = document.querySelectorAll('collapsed')
+const tittle = document.querySelectorAll('collapse')
+
+// main functionality 
+caollapsed.forEach(collap => {
+   console.log(collap)
+})
